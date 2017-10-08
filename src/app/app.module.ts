fac3b2188e30933436bb32939e98fa88086716aa
François <francoischandebois@gmail.com>
2017-10-08 20:33:30 +0200

    add app component, app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,36 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { Router } from '@angular/router';
 import { CommonsModule } from './common/common.modulde';
+import {ComicsListComponent} from "./pages/comics/comics-list/comics-list.component";
+import {ComicsService} from "./core/services/comics.service";
+import {HttpService} from "./core/services/http.service";
+import {HttpFactory} from "./core/core.module";
+import {HttpModule, XHRBackend} from "@angular/http";
+import {AlertService} from "./core/services/alert/alert.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComicsListComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CommonsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    ComicsService,
+    HttpService,
+    AlertService,
+    {
+      provide: HttpService,
+      useFactory: HttpFactory,
+      deps: [XHRBackend, Router, AlertService],
+
+
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {

b95494fa729ccefb55353e9e45b1a1a66a04ff6b
François <francoischandebois@gmail.com>
2017-10-08 20:34:08 +0200

    add environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,8 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiEndpoint: 'http://gateway.marvel.com/v1/public/comics?',
+  AppName: 'Marvel',
+  secret: '',
 };
